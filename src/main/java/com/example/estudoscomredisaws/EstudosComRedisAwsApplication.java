a98da9d8f6a013d4a68a9821c2ad03c392e60826
unknown <lucascarrilho26@gmail.com>
2022-12-08 09:36:54 -0300

    testando a base

---
+++ src/main/java/com/example/estudoscomredisaws/EstudosComRedisAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.estudoscomredisaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosComRedisAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudosComRedisAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/estudoscomredisaws/EstudosComRedisAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estudoscomredisaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosComRedisAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
